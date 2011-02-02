32880d1c95ba899838b4d47361ddb7a8f44a6077
Harris Novick <harris@harrisnovick.com>
2011-02-01 20:03:24 -0500

    Removed itunes ratings to prepare for better measure of quality

--- db/migrate/20110201011428_add_itunes_rating_to_podcasts.rb
+++
@@ -1,9 +0,0 @@
-class AddItunesRatingToPodcasts < ActiveRecord::Migration
-  def self.up
-    add_column :podcasts, :itunes_rating, :string
-  end
-
-  def self.down
-    remove_column :podcasts, :itunes_rating
-  end
-end
--- db/migrate/20110201011445_add_itunes_rating_count_to_podcasts.rb
+++
@@ -1,9 +0,0 @@
-class AddItunesRatingCountToPodcasts < ActiveRecord::Migration
-  def self.up
-    add_column :podcasts, :itunes_rating_count, :integer
-  end
-
-  def self.down
-    remove_column :podcasts, :itunes_rating_count
-  end
-end
