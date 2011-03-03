114311db45469c7fb01db54d4bc8514f3b557bfb
Harris Novick <harris@harrisnovick.com>
2011-03-02 22:54:24 -0500

    Began sketching utility methods in Mention.

--- app/models/mention.rb
+++ app/models/mention.rb
@@ -14,4 +14,17 @@ class Mention < ActiveRecord::Base
   def self.mention_logger
     @@mention_logger ||= Logger.new("#{RAILS_ROOT}/log/mention_cron.log", 3, 524288)
   end
+  
+  def self.twitter_search(text, reply)
+    # Initialize a Twitter search
+    search = Twitter::Search.new
+
+    # Find recent tweets using the given params
+    search.containing(text).to(reply).result_type("recent").per_page(3).each do |r|
+      puts "#{r.from_user}: #{r.text}"
+    end
+
+    # Clear the search
+    search.clear
+  end
 end
