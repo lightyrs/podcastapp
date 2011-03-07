f705cf52425c5aaabaee12193db40897d2004f10
Harris Novick <harris@harrisnovick.com>
2011-03-07 00:34:52 -0500

    Added methods to harvest the relevant mentions from the captured social streams.

--- app/models/mention.rb
+++ app/models/mention.rb
@@ -36,26 +36,85 @@ class Mention < ActiveRecord::Base
     end
   end
   
-  def self.twitter_engine
-    collection = Podcast.find(:all, :select => 'id, name', :conditions => ["id between ? and ?", 0, 400])
-    query = collection.map { |q|
-      name = q.name[0..24]
-      length = name.split(" ").length - 2
-      name = name.split(" ")[0..length].join(" ")
-      unless name.include? "podcast"
-        name + " podcast"
-      end
-    }
-    puts "#{query}"
+  # Harvest salient data from our log files
+  def self.harvest_mentions
+    twitter_logs = Mention.get_recursive_file_list("#{RAILS_ROOT}/log/twitter")
+    facebook_logs = Mention.get_recursive_file_list("#{RAILS_ROOT}/log/facebook")
+    logs = twitter_logs.concat(facebook_logs)
     
-    # Open up a twitter firehose with our query
-    TweetStream::Client.new('theMTA', 'matt22').on_error do |message|
-      puts "#{message}"
-    end.track(query.join(",")) do |status|
-      # Append the tweet to a local file. Create one if the file doesn't exist.
-      current = Time.now.to_s.slice(0..13).gsub(":", "").gsub(" ", "_") + ".log"
-      File.open("#{RAILS_ROOT}/log/twitter/mentions/#{current}", 'a') {|f| f.write(status.text + "\n") }
-      puts "#{status.text}"
-    end    
+    mentions = Mention.mentions_array_builder(logs)
+    Mention.filter_mentions(mentions.uniq)
+  end
+  
+  # Prepare the array of mentions for parsing
+  def self.mentions_array_builder(logs) 
+    decoder = "CX3NP47VNND:  "
+    
+    raw = []
+    logs.each do |log|
+      if log.include? "facebook"
+        network = "facebook"
+      elsif log.include? "twitter"
+        network = "twitter"
+      end
+      File.open(log, "r").each do |line|
+        raw << line.gsub(decoder, "").strip.insert(0, "#" + network + "# ")
+      end
+    end
+    raw = raw.split(decoder)[0]
+  end
+  
+  # Find mentions of specific podcasts
+  def self.filter_mentions(mentions)
+    podcasts = Podcast.find(:all, :select => ["id, name"], :conditions => ["name IS NOT ?", nil])
+  
+    podcasts.each do |pod|
+      begin
+        pod_name = pod.name[0..30]
+        if pod_name.length < 30
+          pod_name = pod_name.downcase
+        else
+          length = pod_name.split(" ").length - 2
+          pod_name = pod_name.split(" ")[0..length].join(" ").downcase
+        end
+    
+        mentions.each do |mention|
+          if mention.downcase.include? "#{pod_name}"
+            mention = mention.split("#")
+            network = mention[1]
+            
+            Mention.create(
+              :mention => mention[2],
+              :network => network,
+              :podcast_id => pod.id
+            )
+            puts "#{mention[2]}"
+            sleep(0.0500)
+          end
+        end
+      rescue StandardError => ex
+        puts "#{ex.class}: #{ex.massage}"
+      end
+    end
+  end
+  
+  # Crawl the log directory and return a list of all files
+  def self.get_recursive_file_list(dirname)
+    results = Array.new
+    
+    use_method = 0
+    if use_method == 0
+      Dir["#{dirname}/**/**"].each do | thisfile |
+        thisfile.gsub!(/\//,'/')
+        results.push (thisfile)
+      end
+    else
+      require 'find'
+      Find.find ( dirname ) do | thisfile |
+        thisfile.gsub!(/\//,'\\')
+        results.push (thisfile)
+      end
+    end
+    return results
   end
 end
