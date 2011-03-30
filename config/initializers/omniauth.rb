2984ff83a07228efe2b045f9a30bdf6172ad8ffb
lightyrs <harris@harrisnovick.com>
2011-03-29 21:13:53 -0400

    Began third-party authentication with omniauth + devise.

---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,5 @@
+require 'openid/store/filesystem'
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :twitter, '785ZIydpfGOllGyA8e0jCQ', 'ZQjzPjDAJ7dqFrsKms5BhCnqMKxcTgSvkWnov9U'
+  provider :facebook, '168623136520386', '1da22ec4168394094d2b30b79b1b93eb'
+end
